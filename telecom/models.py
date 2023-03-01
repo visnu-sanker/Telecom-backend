ff355809220454e2d4d1fb0c06ad858fe44fa213
Visnu Sanker <visnu.sanker@STLERI.COM>
2023-03-01 19:48:36 +0530

    first commit

---
+++ telecom/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255, unique=True)
+    activeplan = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']   
